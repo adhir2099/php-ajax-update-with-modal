3b6c73fde03a5836c8e5bc33a7f8acd611817821
Adhir DC <adhir.serrano@gmail.com>
2022-05-13 08:49:35 -0500

    Initial commit

---
+++ employeeController.php
@@ -0,0 +1,92 @@
+<?php
+
+include('./dbcon.php');
+
+if(isset($_POST["action"])){
+
+    //Load data
+    if($_POST["action"] == "Load"){
+
+        $statement = $connect->prepare("SELECT * FROM employees ORDER BY name ASC");
+        $statement->execute();
+        $result = $statement->fetchAll();
+        $output = '';
+        $output .= '
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Action</th>
+                    <th>Name</th>
+                    <th>Address</th>
+                    <th>Gender</th>
+                    <th>Age</th>
+                </tr>
+            </thead>
+            <tbody>
+        ';
+
+        if(sizeof($result) > 0){
+            
+            foreach($result as $row){
+
+                $output .= '
+                    <input type="hidden" name="" value="'.$row["id"].'">
+                    <tr>    
+                        <td><button class="btn btn-warning btn-xs update" id="'.$row["id"].'"><i class="fas fa-file-signature"></i></button></td>
+                        <td>'.$row["name"].'</td>
+                        <td>'.$row['address'].'</td>
+                        <td>'.$row['gender'].'</td>
+                        <td>'.$row['age'].'</td>
+                    </tr>';
+            }
+        }else{
+            $output .= '
+                <tr>
+                    <td align="center">No records found.</td>
+                </tr>';
+        }
+
+        $output .= '</tbody></table>';
+
+        echo $output;
+
+    }
+}
+
+//Fetch single data for display on Modal
+if($_POST["action"] == "Select"){
+
+    $output = array();
+    $statement = $connect->prepare("SELECT * FROM employees WHERE id = '".$_POST["id"]."'  LIMIT 1" );
+    $statement->execute();
+    $result = $statement->fetchAll();
+
+    foreach($result as $row){
+        $output["id"]      = $row["id"];
+        $output["name"]    = $row["name"];
+        $output["address"] = $row["address"];
+        $output["gender"]  = $row["gender"];
+        $output["age"]     = $row["age"];
+    }
+
+    echo json_encode($output);
+}
+
+//Update
+if($_POST["action"] == "Update"){
+    
+    $statement = $connect->prepare("UPDATE employees SET name = :name, address = :address, gender = :gender, age = :age WHERE id = :id ");
+    $result = $statement->execute(
+        array(
+            ':id'      => $_POST["id"],
+            ':name'    => $_POST["name"],
+            ':address' => $_POST["address"],
+            ':gender'  => $_POST["gender"],
+            ':age'     => $_POST["age"]
+        )
+    );
+
+    if(!empty($result)){
+        echo 'recordUpdated';
+    }
+}
---
+++ index.php
@@ -0,0 +1,155 @@
+<html>  
+    <head>
+        <title>Update records on modal with ajax</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="author" content="Adhirsaurio" />
+        <meta name="description" content="Update records on modal with ajax" />
+        <link href="assets/css/styles.css" rel="stylesheet" />
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
+        <script src="assets/js/fontawesome.js"></script>  
+    </head>  
+
+    <body class="d-flex flex-column min-vh-100">  
+
+        <div class="container mb-5">  
+            <div class="mb-5"></div>
+            <div id="action_alert" class="text-center"></div>
+            <div class="mb-5"></div>
+            <div class="table-responsive mb-0" data-pattern="priority-columns" id ="result"></div>    
+        </div>
+        
+        <footer class="footer text-center mt-auto">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p class="lead mb-0">Update records on modal with ajax <a href="https://github.com/adhirsaurio?tab=repositories"><i class="fab fa-github-alt"></i></a></p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Update records on modal with ajax © 2022</small></div>
+        </div>
+
+        <!-- MODAL CREATE -->
+        <div id="employeeModal" class="modal fade" data-keyboard="false" data-backdrop="static">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title"></h4>
+                    </div>
+                    <div class="modal-body">
+                        <div id="action_alert_error" class="text-center"></div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputEmail4">Name</label>
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputEmail4">Address</label>
+                                <input type="text" class="form-control" id="address" name="address" placeholder="Address">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputEmail4">Gender</label>
+                                <input type="text" class="form-control" id="gender" name="Gender" placeholder="gender">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputEmail4">Age</label>
+                                <input type="number" class="form-control" id="age" name="age" placeholder="Age">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="hidden" name="id" id="id" />
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                        <input type="submit" name="action" id="action" class="btn btn-primary" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- MODAL -->
+
+    </body>  
+</html> 
+
+<script>
+    $(document).ready(function(){ 
+        
+        fetchEmployee(); 
+        function fetchEmployee(){
+
+            let action = "Load";
+
+            $.ajax({
+                url : "employeeController.php", 
+                method:"POST", 
+                data:{action:action}, 
+                success:function(data){
+                    $('#result').html(data);
+                }
+            });
+        }
+
+        //Update
+        $(document).on('click', '.update', function(){
+            
+            let id = $(this).attr("id"); 
+            let action = "Select";   
+
+            $.ajax({
+                url:"employeeController.php",   
+                method:"POST",    
+                data:{id:id, action:action},
+                dataType:"json",   
+                success:function(data){
+                    $('#employeeModal').modal('show');   
+                    $('.modal-title').text("Update employee info"); 
+                    $('#action').val("Update");     
+                    $('#id').val(id);     
+                    $('#name').val(data.name);  
+                    $('#address').val(data.address); 
+                    $('#gender').val(data.gender); 
+                    $('#age').val(data.age); 
+                }
+            });
+        });
+
+        //Update employee 
+        $('#action').click(function(){
+
+            let id      = $('#id').val(); 
+            let name    = $('#name').val();
+            let address = $('#address').val();  
+            let gender  = $('#gender').val();  
+            let age     = $('#age').val();  
+            let action  = $('#action').val();  
+
+            if(name != '' && address !='' && gender != '' && age != ''){
+
+                $.ajax({
+                    url : "employeeController.php",   
+                    method:"POST",     
+                    data:{name:name, address:address, gender:gender, age:age, id:id, action:action}, 
+
+                    success:function(data){
+
+                        if(data == 'recordUpdated'){
+                                $('#action_alert').html("<label class='text-success'><div class='alert alert-success' role='alert'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'><b>X</b></button><label>Employee succesfully updated.</label></div></label>");
+                                fetchEmployee();
+                                $('#employeeModal').modal('hide');     
+                        }else{
+                            $('#action_alert_error').html("<label class='text-danger'><div class='alert alert-danger' role='alert'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'><b>X</b></button><label>Error updating record.</label></div></label>");
+                        }
+                    }
+                });
+            }else{
+                $('#action_alert_error').html("<label class='text-danger'><div class='alert alert-danger' role='alert'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'><b>X</b></button><label>No empty values allowed.</label></div></label>");
+            }
+        });
+
+    });  
+</script>
